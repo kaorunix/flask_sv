1c687d9ac61d299788efbd95496c8f75dd3e7944
Kaoru Sato <k.sato@creist.co.jp>
2021-04-29 16:22:46 +0900

    blueprint追加

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-
+from api import api_bp
 
 app = Flask(__name__, static_folder='../../creist_vue_with_flask/dist/static', template_folder='../../creist_vue_with_flask/dist')
+app.register_blueprint(api_bp)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
