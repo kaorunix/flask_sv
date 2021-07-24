b78bbe37503bf0884339009524d7a5e52aa4836c
Kaoru Sato <k.sato@creist.co.jp>
2021-07-24 14:57:13 +0900

    作成と検索のテストコード追加

--- backend/tests/model/test_Account.py
+++ backend/tests/model/test_Account.py
@@ -49,3 +49,48 @@ def test_create():
     }
 
     assert Account.create(account, 999) == True
+    account_dict = {
+        'account_name' : "flask_sv2",
+        'end_on' : "2030-12-31 00:00:00"
+    }
+    results = Account.search(account_dict, 1)
+    assert results[0].account_name == account_dict['account_name'] 
+    assert results[0].start_on == datetime.datetime.strptime(account['start_on'], '%Y-%m-%d %H:%M:%S')
+    assert results[0].end_on == datetime.datetime.strptime(account_dict['end_on'], '%Y-%m-%d %H:%M:%S')
+    assert results[0].created_by == 999
+    assert results[0].status == Status.getStatusKey("NEW")
+    
+
+def test_update():
+    """
+    """
+    account = {
+        'account_name' : 'flask_sv3',
+        'start_on' : '2021-05-05 00:00:00',
+        'end_on' : '2030-12-31 00:00:00',
+    }
+
+    assert Account.create(account, 999) == True
+    account_dict = {
+        'account_name' : "flask_sv3",
+        'end_on' : "2030-12-31 00:00:00"
+    }
+    results = Account.search(account_dict, 1)
+    account_id = results[0].id
+    result = Account.getById(account_id, 999)
+    account_update = {
+        'id' : account_id,
+        'account_name' : 'flask_sv4',
+        'start_on' : '2021-05-06 00:00:00',
+        'end_on' : '2031-01-01 00:00:00',
+        'status' : results[0].status + 1
+    }
+    assert Account.update(account_update, 999) == True
+    result = Account.getById(account_id, 999)
+    assert result.account_name == account_update['account_name']
+    assert result.start_on == datetime.datetime.strptime(account_update['start_on'], '%Y-%m-%d %H:%M:%S')
+    assert result.end_on == datetime.datetime.strptime(account_update['end_on'], '%Y-%m-%d %H:%M:%S')
+    assert result.created_by == 999
+    assert result.status == account_update['status']
+    
+    
