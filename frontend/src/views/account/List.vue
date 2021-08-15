0bba1689c3f5749336d370f633eb6d7b79e5d68e
Kaoru Sato <k.sato@creist.co.jp>
2021-08-15 18:37:55 +0900

    アカウント一覧画面表示機能実装

--- frontend/src/views/account/List.vue
+++ frontend/src/views/account/List.vue
@@ -5,32 +5,32 @@
     <h3>アカウント一覧</h3>
     <v-simple-table>
       <template v-slot;default>
-		<thead>
-	  	<tr>
-	    <th>アカウントID</th>
-	    <th>アカウント名称</th>
-	    <th>有効開始日</th>
-	    <th>有効終了日</th>
-	    <th>作成者</th>
-	    <th>作成日時</th>
-	    <th>更新者</th>
-	    <th>更新日時</th>
-	    <th>ステータス</th>
-	  	</tr>
-		</thead>
-	<tbody>
-	  <tr>
-	    <td>アカウントID(id)</td>
-	    <td>アカウント名称(account_name)</td>
-	    <td>有効開始日</td>
-	    <td>有効終了日</td>
-	    <td>作成者</td>
-	    <td>作成日時</td>
-	    <td>更新者</td>
-	    <td>更新日時</td>
-	    <td>ステータス</td>
-	  </tr>
-	</tbody>
+        <thead>
+          <tr>
+            <th>アカウントID</th>
+            <th>アカウント名称</th>
+            <th>有効開始日</th>
+            <th>有効終了日</th>
+            <th>作成者</th>
+            <th>作成日時</th>
+            <th>更新者</th>
+            <th>更新日時</th>
+            <th>ステータス</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="account in accounts" v-bind:key="account">
+            <td>{{ account.id }}</td>
+            <td>{{ account.account_name }}</td>
+            <td>{{ account.start_on }}</td>
+            <td>{{ account.end_on }}</td>
+            <td>{{ account.created_by }}</td>
+            <td>{{ account.created_at}} </td>
+            <td>{{ account.updated_by }}</td>
+            <td>{{ account.updated_at }}</td>
+            <td>{{ account.status }}</td>
+          </tr>
+        </tbody>
       </template>
     </v-simple-table>
   </div>
@@ -39,12 +39,37 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
+var accounts = [
+	{
+		id: 123,
+		account_name: "flask_sv_front",
+		start_on: "2021/02/01 10:00:00",
+		end_in: "2021/11/30 18:00:00",
+		created_by: 10, 
+		created_at: "2021/08/25 12:00:00",
+		updated_by: 10, 
+		updated_at: "2021/08/25 12:00:00",
+		status: 0
+	},
+	{
+		id: 124,
+		account_name: "yamada",
+		start_on: "2021/05/01 10:00:00",
+		end_in: "2021/10/30 18:00:00",
+		created_by: 12, 
+		created_at: "2021/08/25 12:00:00",
+		updated_by: 10, 
+		updated_at: "2021/08/25 12:00:00",
+		status: 1
+	}
+]
+
 export default {
-  name: 'SS',
-  data () {
-    return {
-      message: '出力メッセージ'
-    }
-  }
-}
+  name: "SS",
+  data() {
+	  return {
+		  accounts: accounts
+	  }
+  },
+};
 </script>
