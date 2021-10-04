507fe27b160c4dd83a14e3fb4f7fbd0c83b268c8
Kaoru Sato <k.sato@creist.co.jp>
2021-10-04 17:04:50 +0900

    qiita-chapter3 始める前の真っ新な状態

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -17,31 +17,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/account',
-    name: 'アカウント一覧',
-    component: () => import('../views/account/List.vue')
-  },
-  {
-    path: '/account/get',
-    name: 'アカウント詳細',
-    component: () => import('../views/account/Get.vue')
-  },
-  {
-    path: '/account/create',
-    name: 'アカウント作成',
-    component: () => import('../views/account/Create.vue')
-  },
-  {
-    path: '/account/update',
-    name: 'アカウント更新',
-    component: () => import('../views/account/Update.vue')
-  },
-  {
-    path: '/account/delete',
-    name: 'アカウント削除',
-    component: () => import('../views/account/Delete.vue')
   }
 ]
 
