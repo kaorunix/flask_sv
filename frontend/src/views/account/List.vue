2da6a2c693040e6e136893ab9d906457fe693f41
Kaoru Sato <k.sato@creist.co.jp>
2021-10-07 00:36:45 +0900

    Dummyデータで一覧表示

---
+++ frontend/src/views/account/List.vue
@@ -0,0 +1,75 @@
+
+<template>
+  <div class="about">
+    <p>{{ message }}</p>
+    <h3>アカウント一覧</h3>
+    <v-simple-table>
+      <template v-slot;default>
+	<thead>
+	  <tr>
+	    <th>アカウントID</th>
+	    <th>アカウント名称</th>
+	    <th>有効開始日</th>
+	    <th>有効終了日</th>
+	    <th>作成者</th>
+	    <th>作成日時</th>
+	    <th>更新者</th>
+	    <th>更新日時</th>
+	    <th>ステータス</th>
+	  </tr>
+	</thead>
+        <tbody>
+          <tr v-for="account in accounts" v-bind:key="account">
+            <td>{{ account.id }}</td>
+            <td>{{ account.account_name }}</td>
+            <td>{{ account.start_on }}</td>
+            <td>{{ account.end_on }}</td>
+            <td>{{ account.created_by }}</td>
+            <td>{{ account.created_at}} </td>
+            <td>{{ account.updated_by }}</td>
+            <td>{{ account.updated_at }}</td>
+            <td>{{ account.status }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+<script>
+
+var accounts = [
+    {
+        id: 123,
+        account_name: "macbeth",
+        start_on: "2021/02/01 10:00:00",
+        end_in: "2021/11/30 18:00:00",
+        created_by: 10,
+        created_at: "2021/08/25 12:00:00",
+        updated_by: 10,
+        updated_at: "2021/08/25 12:00:00",
+        status: 0
+    },
+    {
+        id: 124,
+        account_name: "duncan",
+        start_on: "2021/05/01 10:00:00",
+        end_in: "2021/10/30 18:00:00",
+        created_by: 12,
+        created_at: "2021/08/25 12:00:00",
+        updated_by: 10,
+        updated_at: "2021/08/25 12:00:00",
+        status: 1
+    }
+]
+
+export default {
+    name: "List",
+    data() {
+        return {
+            accounts: accounts,
+            message: '出力メッセージ'
+        }
+    },
+};
+
+</script>
