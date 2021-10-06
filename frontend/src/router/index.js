80e747d8ea8cbe1fbdf66780a334e909740ff52f
Kaoru Sato <k.sato@creist.co.jp>
2021-10-06 23:20:23 +0900

    ルーティング登録

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,7 +16,17 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },    
+  {
+    path: '/account',
+    name: 'アカウント一覧',
+    component: () => import('../views/account/List.vue')
+  },
+  {
+    path: '/account/create',
+    name: 'アカウント作成',
+    component: () => import('../views/account/Create.vue')
   }
 ]
 
