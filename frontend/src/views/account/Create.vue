ad36008973d846b080c3775431a4338a7675b3e6
Kaoru Sato <k.sato@creist.co.jp>
2021-10-12 23:33:48 +0900

    日付項目にdate-pickerを実装

--- frontend/src/views/account/Create.vue
+++ frontend/src/views/account/Create.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="about">
     <p>{{ message }}</p>
@@ -23,6 +22,9 @@
           <tr>
             <th>有効開始日</th>
             <td>
+              <v-menu v-model="menu" max-width="290px" min-width="290px">
+                <!-- ポップアップを追加する要素にv-on="on" -->
+                <template v-slot:activator="{ on }">
                   <v-text-field
                     slot="activator"
                     v-model="start_on"
@@ -31,11 +33,17 @@
                     readonly
                     v-on="on"
                   ></v-text-field>
+                </template>
+                <v-date-picker v-model="start_on"></v-date-picker>
+              </v-menu>
             </td>
           </tr>
           <tr>
             <th>有効終了日</th>
             <td>
+              <v-menu v-model="menu" max-width="290px" min-width="290px">
+                <!-- ポップアップを追加する要素にv-on="on" -->
+                <template v-slot:activator="{ on }">
                   <v-text-field
                     slot="activator"
                     v-model="end_on"
@@ -44,6 +52,9 @@
                     readonly
                     v-on="on"
                   ></v-text-field>
+                </template>
+                <v-date-picker v-model="end_on"></v-date-picker>
+              </v-menu>
             </td>
           </tr>
           <tr>
@@ -61,8 +72,8 @@
           </tr>
         </tbody>
       </v-simple-table>
-      <v-btn class="mr-4">保存</v-btn>
-      <v-btn>リセット</v-btn>
+      <v-btn class="mr-4" @click="submit">保存</v-btn>
+      <v-btn @click="reset">リセット</v-btn>
     </v-form>
   </div>
 </template>
